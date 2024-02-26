9df20c35e80bf451351544795bbfc9bc92d9bcd6
Yahya <yahyaabbakar@gmail.com>
2024-02-26 23:41:10 +0200

    added dot background component

--- app/page.jsx
+++ app/page.jsx
@@ -1,9 +1,10 @@
+import { DotBackground } from '@/components/DotBackground'
+
 export default function page() {
     return (
-        <section className='relative px-3 py-5 lg:px-10'>
-            <div className='container mx-auto px-4'>
-                <h1 className='text-3xl font-bold'>Next Template</h1>
-            </div>
+        <section className='container relative mx-auto h-screen px-7 py-5 lg:px-10'>
+            <DotBackground />
+            <h1 className='text-3xl font-bold'>Next Template</h1>
         </section>
     )
 }
