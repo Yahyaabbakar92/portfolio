b926e2d2e062e3823b72de5c70403ecf353afdbe
Yahya Abbakar <92931746+Yahyaabbakar92@users.noreply.github.com>
2024-02-26 15:18:50 +0200

    Initial commit

---
+++ app/page.jsx
@@ -0,0 +1,7 @@
+export default function page() {
+    return (
+        <section className='relative px-3 py-5 lg:px-10'>
+            <h1 className='text-3xl font-bold'>Next Template</h1>
+        </section>
+    )
+}
