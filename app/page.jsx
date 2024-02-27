f307ecb4d087fcafa0e531eb6bac004ad8f6fb00
Yahya <yahyaabbakar@gmail.com>
2024-02-27 16:01:56 +0200

    added hero styling and content

--- app/page.jsx
+++ app/page.jsx
@@ -2,7 +2,8 @@ import { DotBackground } from '@/components/DotBackground'
 
 export default function page() {
     return (
-        <section className='container relative mx-auto h-screen px-7 py-5 lg:px-10'>
+        <section className='pb-xs max-w-content mx-auto flex flex-col items-center justify-between px-5 sm:px-10 lg:px-20'>
+            {/* Hero Section */}
             <DotBackground />
             <h1 className='text-3xl font-bold'>Next Template</h1>
         </section>
