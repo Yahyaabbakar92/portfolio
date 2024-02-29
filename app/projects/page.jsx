ca0497c8a7163fecc99e11f10b65bc76cd776650
Yahya <yahyaabbakar@gmail.com>
2024-02-29 15:58:08 +0200

    removed projects page

--- app/projects/page.jsx
+++
@@ -1,9 +0,0 @@
-export default function Projects() {
-    return (
-        <section className='relative px-3 py-5 lg:px-10'>
-            <div className='container mx-auto px-4'>
-                <h1>Projects</h1>
-            </div>
-        </section>
-    )
-}
