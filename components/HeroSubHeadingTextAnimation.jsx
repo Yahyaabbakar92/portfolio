b272412d5cf21ebfdc436ac5c142d244a61adf2f
Yahya <yahyaabbakar@gmail.com>
2024-02-29 15:12:50 +0200

    changed transition duration

--- components/HeroSubHeadingTextAnimation.jsx
+++ components/HeroSubHeadingTextAnimation.jsx
@@ -9,7 +9,7 @@ export default function HeroSubHeadingTextAnimation({ children }) {
             animate={{ opacity: 1, y: 0 }}
             transition={{
                 ease: 'easeIn',
-                duration: 1.7
+                duration: 0.8
             }}
             className='md:ml-4/12 md:mr-1/12 relative z-20 ml-24 text-xl transition-all duration-300 sm:max-w-[780px] md:text-2xl lg:text-3xl'
         >
