d00a015be4169ef915c62007887a24c779dfebf3
Yahya <yahyaabbakar@gmail.com>
2024-02-29 15:12:21 +0200

    removed comment

--- components/DotBackground.jsx
+++ components/DotBackground.jsx
@@ -8,14 +8,12 @@ export function DotBackground() {
             <div className='pointer-events-none absolute inset-0 flex items-center justify-center bg-backgroundColor [mask-image:radial-gradient(ellipse_at_center,transparent_20%,black)] dark:bg-backgroundColorDark'></div>
             <div className='flex flex-col'>
                 <HeroHeadingTextAnimation>
-                    {/* <h1 className='relative z-20 bg-gradient-to-b from-backgroundColor to-backgroundColorDark bg-clip-text py-8 text-4xl font-bold text-transparent transition-all duration-300 dark:bg-gradient-to-b dark:from-neutral-200 dark:to-neutral-500 sm:max-w-[520px] sm:text-7xl'> */}
                     <span className='block transform-none opacity-100'>
                         Hey,
                     </span>
                     <span className='block transform-none opacity-100'>
                         Im Yahya
                     </span>
-                    {/* </h1> */}
                 </HeroHeadingTextAnimation>
                 <div className='transform-none opacity-100'>
                     <HeroSubHeadingTextAnimation>
