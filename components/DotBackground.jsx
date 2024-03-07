e2095c119bdefff1f5999219bc81dbcf08b33b0b
Yahya <yahyaabbakar@gmail.com>
2024-03-07 21:59:48 +0200

    changed styles of elements and subheading text

--- components/DotBackground.jsx
+++ components/DotBackground.jsx
@@ -3,23 +3,21 @@ import HeroSubHeadingTextAnimation from './HeroSubHeadingTextAnimation'
 
 export function DotBackground() {
     return (
-        <div className='relative mb-32 flex h-full w-full items-center justify-center bg-backgroundColor px-4 bg-dot-black/[0.2] dark:bg-backgroundColorDark dark:bg-dot-white/[0.2]'>
-            {/* Radial gradient for the container to give a faded look */}
+        <div className='relative mb-32 flex h-full w-full items-center justify-center bg-backgroundColor px-4 bg-dot-black/[0.2] dark:bg-backgroundColorDark dark:bg-dot-white/[0.2] lg:mt-20'>
             <div className='pointer-events-none absolute inset-0 flex items-center justify-center bg-backgroundColor [mask-image:radial-gradient(ellipse_at_center,transparent_20%,black)] dark:bg-backgroundColorDark'></div>
             <div className='flex flex-col'>
                 <HeroHeadingTextAnimation>
-                    <span className='block transform-none opacity-100'>
+                    <span className='block transform-none text-7xl opacity-100 lg:text-8xl '>
                         Hey,
                     </span>
-                    <span className='block transform-none opacity-100'>
+                    <span className='block transform-none text-7xl opacity-100 lg:text-8xl'>
                         I'm Yahya
                     </span>
                 </HeroHeadingTextAnimation>
                 <div className='transform-none opacity-100'>
                     <HeroSubHeadingTextAnimation>
-                        — a web developer and freelancer crafting web apps &
-                        websites that help businesses stand out and attract more
-                        clients.
+                        — a freelance web developer crafting web apps & websites
+                        that help businesses stand out and attract more clients.
                     </HeroSubHeadingTextAnimation>
                 </div>
             </div>
