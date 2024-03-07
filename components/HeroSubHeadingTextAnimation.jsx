0fb74dcf1c8847a707bee786b00cc109099ac2f8
Yahya <yahyaabbakar@gmail.com>
2024-03-07 22:00:43 +0200

    changed style

--- components/HeroSubHeadingTextAnimation.jsx
+++ components/HeroSubHeadingTextAnimation.jsx
@@ -11,7 +11,7 @@ export default function HeroSubHeadingTextAnimation({ children }) {
                 ease: 'easeIn',
                 duration: 0.8
             }}
-            className='md:ml-4/12 md:mr-1/12 relative z-20 ml-24 text-xl transition-all duration-300 sm:max-w-[780px] md:text-2xl lg:text-3xl'
+            className='md:ml-4/12 md:mr-1/12 relative z-20 ml-16 text-3xl transition-all duration-300 sm:max-w-[780px] md:ml-20 lg:ml-24'
         >
             {children}
         </motion.h2>
