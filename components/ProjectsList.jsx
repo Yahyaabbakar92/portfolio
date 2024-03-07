de178b0f20ff45c0970ad24204d9fac50a6c795b
Yahya <yahyaabbakar@gmail.com>
2024-03-07 22:01:17 +0200

    added 2 column bento grid component

--- components/ProjectsList.jsx
+++ components/ProjectsList.jsx
@@ -1,3 +1,13 @@
-export default function ProjectsList() {
-    return <div>ProjectsList</div>
+import { getProjects } from '@/lib/getProjects'
+import { BentoGridTwoColumn } from './BentoGridTwoColumn'
+
+export default async function ProjectsList() {
+    const projects = await getProjects()
+
+    return (
+        <section id='projects'>
+            <h1 className='mb-14 text-5xl font-bold md:mt-14'>My Work</h1>
+            <BentoGridTwoColumn projects={projects} />
+        </section>
+    )
 }
