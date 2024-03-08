61a158862c3dd02920c7959182249c824ee36a24
Yahya <yahyaabbakar@gmail.com>
2024-03-08 16:41:04 +0200

    removed relative class

--- app/about/page.jsx
+++ app/about/page.jsx
@@ -1,6 +1,6 @@
 export default function About() {
     return (
-        <section className='text-textColor dark:text-textColorDark relative px-3 py-5 lg:px-10'>
+        <section className='px-3 py-5 text-textColor dark:text-textColorDark lg:px-10'>
             <div className='container mx-auto px-4'>
                 <h1>About</h1>
             </div>
