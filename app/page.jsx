dfc16f3e7d95a5927bfd8eac99815e044fe896e1
Yahya <yahyaabbakar@gmail.com>
2024-03-08 16:38:12 +0200

    restructered page layout

--- app/page.jsx
+++ app/page.jsx
@@ -3,11 +3,17 @@ import ProjectsList from '@/components/ProjectsList'
 
 export default function Home() {
     return (
-        <main className='pb-xs max-w-content mx-auto flex flex-col items-center justify-between px-5 sm:px-10 lg:px-20'>
-            {/* Hero Section */}
-            <DotBackground />
+        <>
+            <section className='pb-xs max-w-content mx-auto flex flex-col items-center justify-between px-5 sm:px-10 lg:px-20'>
+                {/* Hero Section */}
+                <DotBackground />
+            </section>
             {/* Projects Section */}
-            <ProjectsList />
-        </main>
+            <section className='relative px-3 py-5 text-textColor dark:text-textColorDark lg:px-10'>
+                <div className='container mx-auto px-4'>
+                    <ProjectsList />
+                </div>
+            </section>
+        </>
     )
 }
