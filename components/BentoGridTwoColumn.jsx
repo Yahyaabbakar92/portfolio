bebe24e2b47789c6031703598a9b3898ba1789c8
Yahya <yahyaabbakar@gmail.com>
2024-03-08 15:17:30 +0200

    refactored bento grid components

--- components/BentoGridTwoColumn.jsx
+++ components/BentoGridTwoColumn.jsx
@@ -8,7 +8,6 @@ export function BentoGridTwoColumn({ projects }) {
             {projects.map((project, i) => (
                 <Suspense fallback={<SkeletonCard />} key={i}>
                     <BentoGridItem
-                        // key={i}
                         title={project.title}
                         description={project.description}
                         header={<Skeleton />}
@@ -22,5 +21,5 @@ export function BentoGridTwoColumn({ projects }) {
     )
 }
 const Skeleton = () => (
-    <div className='flex h-full min-h-[6rem] w-full flex-1 rounded-xl   border border-transparent bg-neutral-100  bg-dot-black/[0.2] [mask-image:radial-gradient(ellipse_at_center,white,transparent)] dark:border-white/[0.2] dark:bg-black dark:bg-dot-white/[0.2]'></div>
+    <div className='flex h-full min-h-[6rem] w-full flex-1 rounded-xl border border-transparent bg-neutral-100  bg-dot-black/[0.2] [mask-image:radial-gradient(ellipse_at_center,white,transparent)] dark:border-white/[0.2] dark:bg-black dark:bg-dot-white/[0.2]'></div>
 )
--- components/ui/bento-grid.jsx
+++ components/ui/bento-grid.jsx
@@ -23,25 +23,29 @@ export const BentoGridItem = ({
     url
 }) => {
     return (
-        <Link
-            href={url}
+        <div
             className={cn(
                 'group/bento shadow-input row-span-1 flex flex-col justify-between space-y-4 rounded-xl border border-transparent bg-white p-4 transition duration-200 hover:shadow-xl dark:border-white/[0.2] dark:bg-black dark:shadow-none',
                 className
             )}
         >
-            {header}
-            <div className='transition duration-200 group-hover/bento:-translate-y-2'>
-                <h3 className='my-2 text-2xl font-bold text-textColor dark:text-textColorDark'>
-                    {title}
-                </h3>
-                <p className='text-base text-textColor dark:text-textColorDark'>
-                    {description}
-                </p>
-                <p className='mt-1 text-sm font-light italic text-textColor dark:text-textColorDark'>
-                    {category}
-                </p>
-            </div>
-        </Link>
+            <Link
+                href={url}
+                className='group/bento row-span-1 flex h-full flex-col justify-between space-y-4 transition duration-200'
+            >
+                {header}
+                <div className='transition duration-200 group-hover/bento:-translate-y-2'>
+                    <h3 className='my-2 text-2xl font-bold text-textColor dark:text-textColorDark'>
+                        {title}
+                    </h3>
+                    <p className='text-base text-textColor dark:text-textColorDark'>
+                        {description}
+                    </p>
+                    <p className='mt-1 text-sm font-light italic text-textColor dark:text-textColorDark'>
+                        {category}
+                    </p>
+                </div>
+            </Link>
+        </div>
     )
 }
