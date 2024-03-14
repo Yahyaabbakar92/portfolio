14ff12ea6a16f4df8e14bc83075c60f4170b5241
Yahya <yahyaabbakar@gmail.com>
2024-03-14 15:15:39 +0200

    moved comment

--- app/page.jsx
+++ app/page.jsx
@@ -4,8 +4,8 @@ import ProjectsList from '@/components/ProjectsList'
 export default function Home() {
     return (
         <>
+            {/* Hero Section */}
             <section className='pb-xs max-w-content mx-auto flex flex-col items-center justify-between px-5 sm:px-10 lg:px-20'>
-                {/* Hero Section */}
                 <DotBackground />
             </section>
             {/* Projects Section */}
